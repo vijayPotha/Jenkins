0a3e51a9a068e7c0b35e8cc2d79a9b3b83f53973
pvr86 <pvr86@DESKTOP-7MJKRIB>
2023-02-04 16:31:45 +0530

    initial commit

---
+++ Jenkins_test/src/main/java/com/example/demo/JenkinsTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestApplication.class, args);
+		System.out.println("hellow world");
+	}
+
+}
